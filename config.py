76ee59f7b6093ca0f115f810a42efc7358512b83
Near <jlliu@gizwits.com>
2017-09-25 10:07:20 +0800

    Format code;

--- config.py
+++ config.py
@@ -10,15 +10,18 @@ class Config():
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'data.sqlite')
     ARTICLES_PER_PAGE = 10
-    COMMENTS_PER_PAGE = 6
-    SECRET_KEY = 'secret key to protect from csrf'
-    WTF_CSRF_SECRET_KEY = 'random key for form' # for csrf protection
-    # Take good care of 'SECRET_KEY' and 'WTF_CSRF_SECRET_KEY', if you use the
-    # bootstrap extension to create a form, it is Ok to use 'SECRET_KEY',
+    COMMENTS_PER_PAGE = 8
+    SECRET_KEY = 'secret_key_to_protect_from_csrf'
+    
+    # for csrf protection
+    WTF_CSRF_SECRET_KEY = 'random_key_for_form' 
+
+    # Take good care of 'SECRET_KEY' and 'WTF_CSRF_SECRET_KEY', 
+    # if you use the bootstrap extension to create a form, 
+    # it is Ok to use 'SECRET_KEY',
     # but when you use tha style like '{{ form.name.labey }}:{{ form.name() }}',
     # you must do this for yourself to use the wtf, more about this, you can
     # take a reference to the book <<Flask Framework Cookbook>>.
-    # But the book only have the version of English.
 
     @staticmethod
     def init_app(app):
