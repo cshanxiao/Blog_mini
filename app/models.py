4851c15f66c8623802eb8f653f7f824aa10087e9
Near <jlliu@gizwits.com>
2017-09-30 12:17:31 +0800

    Upgrade requirements;

--- app/models.py
+++ app/models.py
@@ -1,8 +1,8 @@
-#coding: utf-8
+# -*- coding: utf-8 -*-
 import hashlib
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask.ext.login import UserMixin
+from flask_login import UserMixin
 from . import db, login_manager
 
 article_types = {u'开发语言': ['Python', 'Java', 'JavaScript'],
