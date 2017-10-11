f6a67b8e309c74fbae9b62c63bc45aee13849b93
Near <jlliu@gizwits.com>
2017-10-11 10:57:49 +0800

    Format code;

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -5,6 +5,6 @@ from wtforms.validators import DataRequired, Length, Email
 
 
 class LoginForm(FlaskForm):
-    email = StringField(u'电子邮件', validators=[DataRequired(), Length(1, 64),
-                                             Email()])
+    email = StringField(u'电子邮件', 
+                        validators=[DataRequired(), Length(1, 64), Email()])
     password = PasswordField(u'密码', validators=[DataRequired()])
